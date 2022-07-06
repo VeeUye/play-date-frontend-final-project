e892042036ae84c8bce5aa75e90db112b7d85bbc
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-06 16:09:56 +0100

    Create skeleton CreateEvent component & test suite

---
+++ src/__tests__/components/CreateEvent.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { getByTestId, render, screen } from "@testing-library/react";
+import CreateEvent from "../../components/create-event/CreateEvent";
+
+describe("CreateEvent", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<CreateEvent />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("displays the correct title", () => {
+    render(<CreateEvent />);
+    const title = screen.getByText(/Create Event/i);
+
+    expect(title).toBeVisible();
+  });
+});
+
+describe("Create Event Form", () => {
+  it("displays the Event Name field ", () => {
+    render(<CreateEvent />);
+
+    expect(screen.getByLabelText("Create Event")).toBeVisible();
+
+    expect(screen.getByLabelText("Date")).toBeVisible();
+
+    expect(screen.getByLabelText("Time")).toBeVisible();
+
+    expect(screen.getByLabelText("Location")).toBeVisible();
+
+    expect(screen.getByLabelText("Invite")).toBeVisible();
+  });
+});
+
+describe("Create Event Button", () => {
+  it("displays a sign in button", () => {
+    render(<CreateEvent />);
+
+    const signInButton = screen.getByRole("button", { name: /create event/i });
+
+    expect(signInButton).toHaveTextContent(/create event/i);
+  });
+});
---
+++ src/components/create-event/CreateEvent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./create-event.module.css";
+
+const CreateEvent = (props) => {
+  return <div>CreateEvent</div>;
+};
+
+CreateEvent.propTypes = {};
+
+export default CreateEvent;
