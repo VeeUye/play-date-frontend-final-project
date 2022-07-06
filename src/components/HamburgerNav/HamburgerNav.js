dd501acaa0faa5f9aee2a3589d260989825ebf75
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-07-06 22:56:18 +0100

    Built HamburgerNav and css

---
+++ src/components/HamburgerNav/HamburgerNav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./hamburgernav.css";
+import { stack as Menu } from "react-burger-menu";
+
+const HamburgerNav = (props) => {
+  return (
+    // Pass on our props
+    <Menu {...props} right >
+      <a className="bm-item" href="/">
+        My Profile
+      </a>
+
+      <a className="bm-item" href="/createevent">
+        Create Event
+      </a>
+
+      <a className="bm-item" href="/myevents">
+        My Events
+      </a>
+
+      <a className="bm-item" href="/signout">
+        Sign Out
+      </a>
+    </Menu>
+  );
+};
+
+export default HamburgerNav;
