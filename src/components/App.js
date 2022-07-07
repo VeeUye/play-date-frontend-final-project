3c547f98fcbd7778c634e932fc3b2291def43460
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-07-07 17:39:14 +0100

    changed HamburgerNav dir to hamburger-nav dir and upated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import "../styles/App.css";
 import Splash from "./splash/Splash";
 import "../styles/App.css";
 
-import HamburgerNav from "./HamburgerNav/HamburgerNav";
-import "./HamburgerNav/hamburgernav.css";
+import HamburgerNav from "./hamburger-nav/HamburgerNav";
+import "./hamburger-nav/hamburgernav.css";
 
 
 
@@ -15,7 +15,7 @@ function App() {
       <div id={"App"}>
         <HamburgerNav className="burgernav" pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
-        <div id={"page-wrap"}>
+        <div id="page-wrap">
           <div className="wrapper">
             <Splash />
           </div>
