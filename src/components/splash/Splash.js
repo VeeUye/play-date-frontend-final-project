51088df82cd03b5fc17b64114ec0f6c1f4f3be43
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-07 09:56:21 +0100

    Display CreateEvent.js on Splash.js

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -3,18 +3,24 @@ import Subheading from "../atoms/subheading/Subheading";
 import Title from "../atoms/title/Title";
 import Image from "../../assets/swinging.svg";
 import Button from "../atoms/button/Button";
+import CreateEvent from "../create-event/CreateEvent";
 import splash from "./splash.module.css";
 import button from "../atoms/button/button.module.css";
 
 const Splash = () => {
   return (
-    <div className={splash.background}>
-      <Title text="Playdate" />
-      <Subheading />
-      <img src={Image} alt="girl on a swing" />
-      <Button className={button.signIn} label="Sign In"></Button>
-      <Button className={button.signUp} label="Sign Up"></Button>
-    </div>
+    <>
+      <div className={splash.background}>
+        <Title text="Playdate" />
+        <Subheading />
+        <img src={Image} alt="girl on a swing" />
+        <Button className={button.signIn} label="Sign In"></Button>
+        <Button className={button.signUp} label="Sign Up"></Button>
+      </div>
+      <div className={splash.background}>
+        <CreateEvent />
+      </div>
+    </>
   );
 };
 
