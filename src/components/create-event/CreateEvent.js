6590c9184ca8f136ff8789d45361472bd795b2b3
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-07 14:06:07 +0100

    Style createEvent button

--- src/components/create-event/CreateEvent.js
+++ src/components/create-event/CreateEvent.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SmallTitle from "../atoms/small-title/SmallTitle";
 import CreateEventForm from "./CreateEventForm";
+import Button from "../atoms/button/Button";
 import Image from "../../assets/images/float.svg";
 import createEventStyles from "../create-event/create-event.module.css";
 import titleStyles from "../atoms/small-title/small-title.module.css";
+import buttonStyles from "../atoms/button/button.module.css";
 
 import "./create-event.module.css";
 
@@ -12,6 +14,7 @@ const CreateEvent = () => {
     <div className="background">
       <SmallTitle className={titleStyles.default} text="Create Event" />
       <CreateEventForm />
+      <Button className={buttonStyles.createEvent} label="Create Event" />
       <img className={createEventStyles.img} src={Image} alt="girl floating" />
     </div>
   );
