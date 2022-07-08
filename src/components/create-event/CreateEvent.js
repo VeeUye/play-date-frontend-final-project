248d49c0bdda4eece72ab1e40c905f721418461b
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-08 21:31:20 +0100

    Add hook to control styling on window resize

--- src/components/create-event/CreateEvent.js
+++ src/components/create-event/CreateEvent.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SmallTitle from "../atoms/small-title/SmallTitle";
 import CreateEventForm from "./CreateEventForm";
 import Image from "../../assets/images/float.svg";
 import createEventStyles from "../create-event/create-event.module.css";
 import titleStyles from "../atoms/small-title/small-title.module.css";
-
 import "./create-event.module.css";
 
 const CreateEvent = () => {
+  const [isSmall, setIsSmall] = useState(window.innerWidth < 600);
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        const issmall = window.innerWidth < 600;
+        if (issmall !== isSmall) setIsSmall(issmall);
+      },
+      false
+    );
+  }, [isSmall]);
   return (
     <>
-      <div className="background">
-        <SmallTitle className={titleStyles.default} text="Create Event" />
-        <CreateEventForm />
+      <div
+        className={
+          isSmall ? createEventStyles.smallScreen : createEventStyles.bigScreen
+        }
+      >
+        <div className="background">
+          <SmallTitle className={titleStyles.default} text="Create Event" />
+          <CreateEventForm />
+          <img
+            className={createEventStyles.img}
+            src={Image}
+            alt="girl floating"
+          />
+        </div>
         <img
-          className={createEventStyles.img}
+          className={createEventStyles.img2}
           src={Image}
           alt="girl floating"
         />
       </div>
-      <img className={createEventStyles.img2} src={Image} alt="girl floating" />
     </>
   );
 };
--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Subheading from "../atoms/subheading/Subheading";
 import Title from "../atoms/title/Title";
 import Image from "../../assets/images/swinging.svg";
@@ -7,14 +7,28 @@ import splash from "./splash.module.css";
 import button from "../atoms/button/button.module.css";
 
 const Splash = () => {
+  const [isSmall, setIsSmall] = useState(window.innerWidth < 600);
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        const issmall = window.innerWidth < 600;
+        if (issmall !== isSmall) setIsSmall(issmall);
+      },
+      false
+    );
+  }, [isSmall]);
+
   return (
     <>
-      <div className={splash.background}>
-        <Title text="Playdate" />
-        <Subheading />
-        <img className={splash.img} src={Image} alt="girl on a swing" />
-        <Button className={button.signIn} label="Sign In"></Button>
-        <Button className={button.signUp} label="Sign Up"></Button>
+      <div className={isSmall ? splash.smallScreen : splash.bigScreen}>
+        <div className={splash.background}>
+          <Title text="Playdate" />
+          <Subheading />
+          <img className={splash.img} src={Image} alt="girl on a swing" />
+          <Button className={button.signIn} label="Sign In"></Button>
+          <Button className={button.signUp} label="Sign Up"></Button>
+        </div>
       </div>
     </>
   );
