fe740f65e8df516eb966787fddd4ad9b8fc9368c
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-09 08:59:55 +0100

    Create CreateProfile component

---
+++ src/components/create-profile/CreateProfile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import createProfileStyles from "./create-profile.module.css";
+
+const CreateProfile = () => {
+  return <div>CreateProfile</div>;
+};
+
+export default CreateProfile;
