07d8f3fff0383a57cab740d8727d721e5d464d2e
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-09 15:53:14 +0100

    Update Title component to take a className prop, add background to SignUp

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -2,8 +2,9 @@ import React from "react";
 import screenSize from "../../functions/screenSize";
 import SignUpForm from "./SignUpForm";
 import Title from "../atoms/title/Title";
+import BgImage from "../../assets/images/wavy2.svg";
 import signUpStyles from "./sign-up.module.css";
-import titleStyles from "../atoms/title/title.module.css";
+import title from "../atoms/title/title.module.css";
 
 const SignUp = () => {
   const isSmall = screenSize();
@@ -13,10 +14,17 @@ const SignUp = () => {
         className={isSmall ? signUpStyles.smallScreen : signUpStyles.bigScreen}
       >
         <div className="background">
-          <Title className={titleStyles.createProfile} text="Create Account" />
-          <SignUpForm />
+          <img
+            className={
+              isSmall ? signUpStyles.smallScreenImg : signUpStyles.bigScreenImg
+            }
+            src={BgImage}
+            alt="abstract white wavy background"
+          />
         </div>
       </div>
+      <Title className={title.signUp__small} text="Create Account" />
+      <SignUpForm />
     </>
   );
 };
