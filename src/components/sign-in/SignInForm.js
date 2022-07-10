fdb10066734ada7ad76f6018233ba9d7aa0777e2
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-10 11:56:02 +0100

    Create basic SignIn and SignInForm, add to Switch

---
+++ src/components/sign-in/SignInForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import FormInput from "../atoms/form-input/FormInput";
+import screenSize from "../../functions/screenSize";
+import Button from "../atoms/button/Button";
+import formStyles from "./sign-in-form.module.css";
+import inputStyles from "../atoms/form-input/form-input.module.css";
+import buttonStyles from "../atoms/button/button.module.css";
+
+const SigninForm = () => {
+  const initialState = {
+    fields: {
+      username: "test name",
+      email: "",
+      password: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleCreateEvent = (event) => {
+    event.preventDefault();
+    console.log(fields);
+    setFields(initialState.fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  const isSmall = screenSize();
+
+  return (
+    <>
+      <form onSubmit={handleCreateEvent}>
+        <div className={formStyles.signUpField1}>
+          <div>
+            <FormInput
+              className={inputStyles.inputSignUp}
+              label="Username"
+              type="text"
+              name="name"
+              value={fields.name}
+              onChange={handleFieldChange}
+            />
+
+            <FormInput
+              className={inputStyles.inputSignUp}
+              label="email"
+              type="email"
+              name="email"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+
+            <FormInput
+              className={inputStyles.inputSignUp}
+              label="Password"
+              type="password"
+              name="password"
+              value={fields.password}
+              onChange={handleFieldChange}
+            />
+            <div
+              className={
+                isSmall
+                  ? formStyles.buttonWrapper
+                  : formStyles.bigScreenButtonWrapper
+              }
+            >
+              <Button
+                className={buttonStyles.signUp2}
+                type="submit"
+                label="Sign Up"
+              />
+              <Button
+                className={buttonStyles.signIn2}
+                type="submit"
+                label="Sign In"
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SigninForm;
