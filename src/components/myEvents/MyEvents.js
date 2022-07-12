cd7a869612e90fef719e65ee038aa64973e21ddf
Steven Lomax <ste.lomax@gmail.com>
2022-07-12 15:49:01 +0100

    added EventCard styling modules

--- src/components/myEvents/MyEvents.js
+++ src/components/myEvents/MyEvents.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import EventCard from "./EventCard";
+import SmallTitle from "../atoms/small-title/SmallTitle";
+import titleStyles from "../atoms/small-title/small-title.module.css";
 
 const MyEvents = ({ events }) => {
     return (
         <div className="myEvents">
-            MyEvents
+             <SmallTitle className={titleStyles.default} text="My Events" />
             {events.map(events => (
                 <EventCard 
                 key={events.owner}
