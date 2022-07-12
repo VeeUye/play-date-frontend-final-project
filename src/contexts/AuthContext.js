4d3c57963a82067f906a74ce983e177f18e39b9d
Ed Adkins <ejmadkins@gmail.com>
2022-07-12 12:38:58 +0100

    add sign out functionality

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,8 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
   //   signInWithEmailAndPassword,
   //   signOut,
   //   onAuthStateChanged,
@@ -11,12 +13,29 @@ const UserContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      console.log(user);
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <UserContext.Provider value={{ createUser }}>
+    <UserContext.Provider value={{ createUser, user, logout }}>
       {children}
     </UserContext.Provider>
   );
