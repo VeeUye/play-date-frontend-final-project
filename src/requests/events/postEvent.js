eef4b0e5f35d83e96625450452756fa17b4836ce
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-13 12:11:31 +0100

    Update request syntax

--- src/requests/events/postEvent.js
+++ src/requests/events/postEvent.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
-// Current erroring with bad request. Need to match req data with main
+export const BASE_URL =
+  "http://localhost:5001/mc-play-date-scheduler/europe-west2/app/";
 
-const postEvent = (fields) => {
-  axios
-    .post(
-      "http://localhost:5001/mc-play-date-scheduler/europe-west2/app/events",
-      fields
-    )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const postEvent = async (fields) => {
+  try {
+    const res = await axios.post(`${BASE_URL}/events`, fields);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default postEvent;
