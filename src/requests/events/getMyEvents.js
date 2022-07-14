fa87b8b69353fb5e3baacf36182c150a25b1d8eb
Steven Lomax <ste.lomax@gmail.com>
2022-07-14 22:48:53 +0100

    changed margins to improve layout on my events

--- src/requests/events/getMyEvents.js
+++ src/requests/events/getMyEvents.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
-export const BASE_URL =
-  "http://localhost:5001/mc-play-date-scheduler/europe-west2/app";
+const BASE_URL =
+  process.env.REACT_APP_FIREBASE_FIRESTORE_URL ||
+  "https://europe-west2-mc-play-date-scheduler.cloudfunctions.net/app";
 
-const getMyEvents = () => {
-    axios
-}
+const getMyEvents = async(user, userIdToken) => {
+    const tokenResult = await userIdToken
+    
+    return axios
+    .get(`${BASE_URL}/events/user-events/${user}/friends`, {
+      headers: { Authorization: `Bearer ${tokenResult}` },
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export default getMyEvents
