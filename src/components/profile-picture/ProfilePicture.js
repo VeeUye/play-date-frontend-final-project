84f0995df97979c4c3dc6a1154f64c92af1436e0
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-07-15 14:27:47 +0100

    Style submit button

--- src/components/profile-picture/ProfilePicture.js
+++ src/components/profile-picture/ProfilePicture.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { storage } from "../../firebase";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import profilePictureStyles from "./profile-picture.module.css";
+import buttonStyles from "../atoms/button/button.module.css";
 
 const ProfilePicture = () => {
   const [imgUrl, setImgUrl] = useState(null);
   const [progresspercent, setProgresspercent] = useState(0);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const file = e.target[0]?.files[0];
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const file = event.target[0]?.files[0];
     if (!file) return;
     const storageRef = ref(storage, `files/${file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
@@ -34,10 +36,6 @@ const ProfilePicture = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="form">
-        <input type="file" />
-        <button type="submit">Upload</button>
-      </form>
       {!imgUrl && (
         <div className="outerbar">
           <div className="innerbar" style={{ width: `${progresspercent}%` }}>
@@ -45,7 +43,20 @@ const ProfilePicture = () => {
           </div>
         </div>
       )}
-      {imgUrl && <img src={imgUrl} alt="uploaded file" height={200} />}
+      {imgUrl && (
+        <img
+          className={profilePictureStyles.profileImg}
+          src={imgUrl}
+          alt="uploaded file"
+          height={200}
+        />
+      )}
+      <form onSubmit={handleSubmit} className="form">
+        <input type="file" />
+        <button className={buttonStyles.pictureUpload} type="submit">
+          Upload
+        </button>
+      </form>
     </div>
   );
 };
