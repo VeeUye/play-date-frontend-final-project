9a3f9f9fef4f47a2295db9dbb410e890caddb77d
Paul Beard <pbeardey@gmail.com>
2022-07-16 23:51:04 +0100

    change props etc to use live event data instead of dummy data

--- src/components/myEvents/EventCard.js
+++ src/components/myEvents/EventCard.js
@@ -6,16 +6,19 @@ import Button from "../atoms/button/Button";
 import buttonStyles from "../atoms/button/button.module.css";
 import { Icon } from "@iconify/react";
 
-const EventCard = ({
-  // owner,
-  //   date,
-  startTime,
-  endTime,
-  location,
-  name,
-  // friendsConfirmed,
-  // friendsInvited
-}) => {
+const EventCard = ( {eventData} ) => {
+  const { date_start, date_end, location } = eventData;
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const d_start = new Date(date_start);
+  const d_end = new Date(date_end);
+  const d_start_date = d_start.getDate();
+  const d_start_month = month[d_start.getMonth()];
+  const d_start_hours = d_start.getHours();
+  const d_start_minutes = d_start.getMinutes();
+  const d_end_hours = d_end.getHours();
+  const d_end_minutes = d_end.getMinutes();
+
+  console.log(eventData);
   return (
     <div className={eventCardStyles.background}>
       <div className={eventCardStyles.details}>
@@ -23,8 +26,8 @@ const EventCard = ({
         {/* Commenting out current date as is not being passed correct format for date */}
         {/* <div className={eventCardStyles.date}>{date}</div> */}
         {/* comment end */}
-        <div className={eventCardStyles.day}>08</div>
-        <div className={eventCardStyles.month}>July</div>
+        <div className={eventCardStyles.day}>{d_start_date}</div>
+        <div className={eventCardStyles.month}>{d_start_month}</div>
         <div className={eventCardStyles.iconLocation}>
           <Icon
             className={eventCardStyles.icon}
@@ -35,7 +38,7 @@ const EventCard = ({
         <div className={eventCardStyles.iconTime}>
           <Icon className={eventCardStyles.icon} icon="bx:time-five" />
           <div className={eventCardStyles.startTime}>
-            {startTime} - {endTime}
+            {d_start_hours}:{d_start_minutes} - {d_end_hours}:{d_end_minutes}
           </div>
         </div>
         <div className={eventCardStyles.iconName}>
@@ -57,14 +60,15 @@ const EventCard = ({
 };
 
 EventCard.propTypes = {
-  owner: PropTypes.number.isRequired,
-  date: PropTypes.string.isRequired,
-  startTime: PropTypes.string.isRequired,
-  endTime: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  friendsConfirmed: PropTypes.arrayOf(PropTypes.number),
-  friendsInvited: PropTypes.arrayOf(PropTypes.number),
+  eventData: PropTypes.shape({
+    // owner: PropTypes.string.isRequired,
+    date_start: PropTypes.string.isRequired,
+    date_end: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    // name: PropTypes.string.isRequired,
+    // friendsConfirmed: PropTypes.arrayOf(PropTypes.string),
+    // friendsInvited: PropTypes.arrayOf(PropTypes.string),
+  })
 };
 
 export default EventCard;
