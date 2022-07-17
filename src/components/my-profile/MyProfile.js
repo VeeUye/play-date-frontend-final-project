dbce961ee85248da22003c8eedcbe923be75c754
Ed Adkins <ejmadkins@gmail.com>
2022-07-17 17:58:14 +0100

    add loading spinner to MyProfile component to survive reloads

--- src/components/my-profile/MyProfile.js
+++ src/components/my-profile/MyProfile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import getMyProfile from "../../requests/profile/getMyProfile";
 import { UserAuth } from "../../contexts/AuthContext";
+import LoadSpinner from "../load-spinner/LoadSpinner";
 
 import Image from "../../assets/images/avatar.svg";
 import Image1 from "../../assets/images/friend1.svg";
@@ -15,112 +16,161 @@ import superSubstyles from "../atoms/supersubheading/supersubheading.module.css"
 
 import myProfileStyles from "../my-profile/my-profile.module.css";
 
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 import Button from "../atoms/button/Button";
 import buttonStyles from "../atoms/button/button.module.css";
 
-
 const MyProfile = () => {
-    const history = useHistory();
-    const { user, token } = UserAuth();
+  const history = useHistory();
+  const { user, token } = UserAuth();
 
-    const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState();
 
-    useEffect(() => {
-        // console.log(user.uid);
-        getMyProfile(user.uid, token)
-        .then((userResults) => {
-            setUserData(userResults)
-        })
+  useEffect(() => {
+    getMyProfile(user.uid, token).then((userResults) => {
+      setUserData(userResults);
+    });
+  }, [user]);
 
-    }, []);
+  const handleCreateEvent = () => {
+    history.push("/create-event");
+  };
 
-    const children = userData.children;
-    const child = children.join();
+  const handleMyEvents = () => {
+    history.push("/my-events");
+  };
 
-    const handleCreateEvent = () => {
-        history.push("/create-event");
-    };
+  const handleEditProfile = () => {
+    history.push("/edit-profile");
+  };
 
-    const handleMyEvents = () => {
-        history.push("/my-events");
-    };
-
-    const handleEditProfile = () => {
-        history.push("/edit-profile");
-    };
-
-    return (
+  return (
+    <>
+      {!userData ? (
+        <LoadSpinner />
+      ) : (
         <div className={myProfileStyles.background}>
+          <div className={myProfileStyles.hero}></div>
 
-            <div className={myProfileStyles.hero}>
+          <div className={myProfileStyles.profileCard}>
+            <img
+              className={myProfileStyles.avatarImage}
+              src={Image}
+              alt="user profile picture"
+            />
+            <SmallTitle
+              className={myProfileStyles.smallTitle}
+              text={userData.name}
+            />
+            <Button
+              className={buttonStyles.myProfile3}
+              label="EDIT PROFILE"
+              onClick={handleEditProfile}
+            ></Button>
+          </div>
+
+          <div className={myProfileStyles.card1}>
+            <div className={myProfileStyles.cardItem}>
+              <Icon
+                className={myProfileStyles.icon}
+                icon="carbon:location-filled"
+              />
+              <SuperSubHeading
+                className={superSubstyles.myProfile}
+                text={userData.location}
+              />
             </div>
 
-
-            <div className={myProfileStyles.profileCard}>
-                <img className={myProfileStyles.avatarImage} src={Image} alt="user profile picture" />
-                <SmallTitle className={myProfileStyles.smallTitle} text={userData.name} />
-                <Button className={buttonStyles.myProfile3} label="EDIT PROFILE" onClick={handleEditProfile}></Button>
+            <div className={myProfileStyles.cardItem}>
+              <Icon className={myProfileStyles.icon} icon="cil:child" />
+              <SuperSubHeading
+                className={superSubstyles.myProfile}
+                text={userData.children}
+              />
             </div>
+          </div>
 
-            <div className={myProfileStyles.card1}>
-                <div className={myProfileStyles.cardItem}>
-                    <Icon className={myProfileStyles.icon} icon="carbon:location-filled" />
-                    <SuperSubHeading className={superSubstyles.myProfile} text={userData.location} />
-                </div>
+          <div className={myProfileStyles.buttons}>
+            <Button
+              className={buttonStyles.myProfile1}
+              label="CREATE EVENT"
+              onClick={handleCreateEvent}
+            ></Button>
 
-                <div className={myProfileStyles.cardItem}>
-                    <Icon className={myProfileStyles.icon} icon="cil:child" />
-                    <SuperSubHeading className={superSubstyles.myProfile} text={child} />
-                </div>
+            <Button
+              className={buttonStyles.myProfile1}
+              label="MY EVENTS"
+              onClick={handleMyEvents}
+            ></Button>
+          </div>
+
+          <div className={myProfileStyles.friendAlertCard}>
+            <img
+              className={myProfileStyles.img2}
+              src={Image2}
+              alt="friend2 profile picture"
+            />
+            <div className={myProfileStyles.card}>
+              <h2>YOU ARE INVITED</h2>
+              <p className={myProfileStyles.pBig}>Seesaw Adventure!</p>
+              <p className={myProfileStyles.p}>16th July 2022 | 9:00 - 12:00</p>
+              <p className={myProfileStyles.p}>Manchester park, Manchester</p>
+              <p className={myProfileStyles.p}>from @bobby1234</p>
+              <div className={myProfileStyles.buttonsAlert}>
+                <Button className={buttonStyles.myProfile2} label="ACCEPT" />
+                <Button className={buttonStyles.myProfile2} label="DECLINE" />
+              </div>
             </div>
+          </div>
 
-            <div className={myProfileStyles.buttons}>
-                <Button className={buttonStyles.myProfile1} label="CREATE EVENT" onClick={handleCreateEvent}></Button>
+          <div className={myProfileStyles.friendList}>
+            <SmallTitle
+              className={myProfileStyles.smallTitle2}
+              text="FRIENDS"
+            />
 
-                <Button className={buttonStyles.myProfile1} label="MY EVENTS" onClick={handleMyEvents}></Button>
+            <div className={myProfileStyles.friendImages}>
+              <img
+                className={myProfileStyles.img1}
+                src={Image1}
+                alt="friend1 profile picture"
+              />
+              <img
+                className={myProfileStyles.img2}
+                src={Image2}
+                alt="friend2 profile picture"
+              />
+              <img
+                className={myProfileStyles.img3}
+                src={Image3}
+                alt="friend3 profile picture"
+              />
             </div>
-
-            <div className={myProfileStyles.friendAlertCard}>
-                <img className={myProfileStyles.img2} src={Image2} alt="friend2 profile picture" />
-                <div className={myProfileStyles.card}>
-                    <h2>YOU ARE INVITED</h2>
-                    <p className={myProfileStyles.pBig}>Seesaw Adventure!</p>
-                    <p className={myProfileStyles.p}>
-                        16th July 2022  | 9:00 - 12:00
-                    </p>
-                    <p className={myProfileStyles.p}>
-                        Manchester park, Manchester
-                    </p>
-                    <p className={myProfileStyles.p}>from @bobby1234</p>
-                    <div className={myProfileStyles.buttonsAlert}>
-                        <Button className={buttonStyles.myProfile2} label="ACCEPT" />
-                        <Button className={buttonStyles.myProfile2} label="DECLINE" />
-                    </div>
-                </div>
+            <div className={myProfileStyles.friendNames}>
+              <SuperSubHeading
+                className={superSubstyles.myProfile}
+                text="Maria"
+              />
+              <SuperSubHeading
+                className={superSubstyles.myProfile}
+                text="Bobby"
+              />
+              <SuperSubHeading
+                className={superSubstyles.myProfile}
+                text="Anna"
+              />
             </div>
-
-            <div className={myProfileStyles.friendList}>
-                <SmallTitle className={myProfileStyles.smallTitle2} text="FRIENDS" />
-
-                <div className={myProfileStyles.friendImages}>
-                    <img className={myProfileStyles.img1} src={Image1} alt="friend1 profile picture" />
-                    <img className={myProfileStyles.img2} src={Image2} alt="friend2 profile picture" />
-                    <img className={myProfileStyles.img3} src={Image3} alt="friend3 profile picture" />
-                </div>
-                <div className={myProfileStyles.friendNames}>
-                    <SuperSubHeading className={superSubstyles.myProfile} text="Maria" />
-                    <SuperSubHeading className={superSubstyles.myProfile} text="Bobby" />
-                    <SuperSubHeading className={superSubstyles.myProfile} text="Anna" />
-                </div>
-            </div>
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
-{/* MyProfile.propTypes = {
+{
+  /* MyProfile.propTypes = {
     name: PropTypes.string
-}; */}
+}; */
+}
 
 export default MyProfile;
-
--- src/requests/profile/getMyProfile.js
+++ src/requests/profile/getMyProfile.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
 const BASE_URL =
-    // eslint-disable-next-line no-undef
-    process.env.REACT_APP_FIREBASE_FIRESTORE_URL ||
-    "https://europe-west2-mc-play-date-scheduler.cloudfunctions.net/app";
+  // eslint-disable-next-line no-undef
+  process.env.REACT_APP_FIREBASE_FIRESTORE_URL ||
+  "https://europe-west2-mc-play-date-scheduler.cloudfunctions.net/app";
 
 const getMyProfile = async (userId, userIdToken) => {
-    const tokenResult = await userIdToken;
-
-
+  if (userId && userIdToken) {
     return axios
-        .get(`${BASE_URL}/users/${userId}`, {
-            headers: { Authorization: `Bearer ${tokenResult}` },
-        })
-        .then((res) => {
-            return res.data;
-            // console.log(res.data);
-        })
-        .catch((err) => {
-            // setAlert({
-            //     message: "Server Error. Could not fetch user data",
-            //     isSuccess: false,
-            // });
-            console.log(err);
-        });
+      .get(`${BASE_URL}/users/${userId}`, {
+        headers: { Authorization: `Bearer ${userIdToken}` },
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        // setAlert({
+        //     message: "Server Error. Could not fetch user data",
+        //     isSuccess: false,
+        // });
+        console.log(err);
+      });
+  }
 };
 
 export default getMyProfile;
