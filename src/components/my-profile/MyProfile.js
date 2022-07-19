a8bf9b82fc4288c2c002594b19e67393f4c9d980
Ed Adkins <ejmadkins@gmail.com>
2022-07-20 00:48:45 +0100

    add styles to add friend

--- src/components/my-profile/MyProfile.js
+++ src/components/my-profile/MyProfile.js
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { UserAuth } from "../../contexts/AuthContext";
 import { InviteResponse } from "../../contexts/InviteContext";
+import Select from "../atoms/form-input/SelectInput";
 import getMyProfile from "../../requests/profile/getMyProfile";
+import getAddFriends from "../../requests/users/getAddFriends";
 import getMyPendingEvents from "../../requests/events/getMyPendingEvents";
 import getUserFriends from "../../requests/users/getUserFriends";
+import editProfile from "../../requests/profile/editProfile";
 import LoadSpinner from "../load-spinner/LoadSpinner";
 import acceptEvent from "../../requests/events/putAcceptEvent";
 import declineEvent from "../../requests/events/putDeclineEvent";
+import addButton from "../../assets/images/circle-button.svg";
+import Alert from "../../requests/alert/Alert";
 
 // import Image from "../../assets/images/avatar.svg";
 // import Image1 from "../../assets/images/friend1.svg";
@@ -27,6 +32,13 @@ import Button from "../atoms/button/Button";
 import buttonStyles from "../atoms/button/button.module.css";
 
 const MyProfile = () => {
+  const initialState = {
+    alert: {
+      message: "",
+      isSuccess: false,
+    },
+  };
+
   const history = useHistory();
   const { user, token } = UserAuth();
   const {
@@ -38,19 +50,51 @@ const MyProfile = () => {
   const [userData, setUserData] = useState([]);
   const [events, setEvents] = useState([]);
   const [userFriend, setUserFriends] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [addFriends, setAddFriends] = useState([]);
   const [eventFriends, setEventFriends] = useState([]);
   const [cardRemoved, setCardRemoved] = useState(0);
+  const [selectedFriend, setSelectedFriend] = useState({});
+  const [alert, setAlert] = useState(initialState.alert);
 
   useEffect(() => {
-    getMyProfile(user.uid, token).then((userResults) => {
-      if (userResults) {
-        setUserData(userResults);
-      }
-    });
-    getUserFriends(user.uid, token).then((userFriendResults) => {
-      setUserFriends(userFriendResults);
-    });
-  }, [user]);
+    getMyProfile(user.uid, token)
+      .then((userResults) => {
+        if (userResults) {
+          setUserData(userResults);
+          return userResults;
+        }
+      })
+      // eslint-disable-next-line no-unused-vars
+      .then((userResults) => {
+        getUserFriends(user.uid, token)
+          .then((userFriendResults) => {
+            setUserFriends(userFriendResults);
+            return userFriendResults;
+          })
+          .then((userFriendResults) => {
+            getAddFriends(token).then((result) => {
+              if (result && userFriendResults) {
+                const filterUsers = result
+                  .filter(
+                    (o1) =>
+                      !userFriendResults.some((o2) => o1.userId === o2.userId)
+                  )
+                  .map((friend) => {
+                    // console.log(friend);
+                    // console.log(userResults);
+                    // if (friend.userId !== userResults.userId) {
+                    // console.log(friend);
+                    return { value: friend.userId, label: friend.name };
+                    // }
+                  });
+
+                setAddFriends(filterUsers);
+              }
+            });
+          });
+      });
+  }, [user, selectedFriend]);
 
   useEffect(() => {
     getMyPendingEvents(setEvents, user.uid, token).then((eventResults) => {
@@ -102,9 +146,23 @@ const MyProfile = () => {
     history.push("/edit-profile");
   };
 
+  const handleSelectFriend = (event) => {
+    // console.log(event);
+    setSelectedFriend({ event });
+  };
+
+  const handleAddFriend = () => {
+    const fields = {
+      ["friends"]: [selectedFriend.event.value, ...userData.friends],
+    };
+    console.log(fields);
+    editProfile(fields, userData.userId, token, setAlert);
+    setSelectedFriend({});
+  };
+
   return (
     <>
-      {!userData || !userFriend || !eventFriends ? (
+      {!userData || !userFriend || !eventFriends || !addFriends ? (
         <LoadSpinner />
       ) : (
         <div className={myProfileStyles.background}>
@@ -211,7 +269,23 @@ const MyProfile = () => {
               className={myProfileStyles.smallTitle2}
               text="FRIENDS"
             />
-
+            <div className={myProfileStyles.friendAddContainer}>
+              <div className={myProfileStyles.friendSearch}>
+                <Select
+                  styles={myProfileStyles.input}
+                  onChange={handleSelectFriend}
+                  options={addFriends}
+                />
+              </div>
+              <div className={myProfileStyles.friendAdd}>
+                <img
+                  src={addButton}
+                  onClick={handleAddFriend}
+                  alt="Add Button"
+                />
+              </div>
+            </div>
+            <Alert message={alert.message} success={alert.isSuccess} />
             <ul className={myProfileStyles.friendList}>
               {/* IMAGE WITH CAPTION USING LIVE DATA BELOW */}
 
