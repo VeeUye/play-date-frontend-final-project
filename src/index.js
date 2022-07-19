1c628662ba82476a9519419ba9db8b26a9aa3bcb
Ed Adkins <ejmadkins@gmail.com>
2022-07-19 15:58:50 +0100

    add InviteContext wrapper to app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
 import { AuthContextProvider } from "./contexts/AuthContext";
-
+import { InviteContextProvider } from "./contexts/InviteContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <InviteContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </InviteContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
